393021c5f18bea40c193ae3e65780221f4fb7551
Scott Clingan <sclingan1@gmail.com>
2023-05-19 14:33:54 -0500

    Adding basic mobile layout, fonts and variables

--- src/form.tsx
+++ src/form.tsx
@@ -1,19 +1,19 @@
 import { useState } from 'react'
-import btn from './assets/images/icon-arrow.svg'
 
 function Form() {
 
   return (
     <main>
+      <h1 className='sr-only'>Age Calculator App</h1>
         <form>
           <label htmlFor='day'>Day</label>
-          <input placeholder='DD'></input>
+          <input placeholder='DD' id='day'></input>
           <label htmlFor='month'>Month</label>
-          <input placeholder='MM'></input>
+          <input placeholder='MM' id='month'></input>
           <label htmlFor='year'>Year</label>
-          <input placeholder='YYYY'></input>
+          <input placeholder='YYYY' id='year'></input>
           <button>
-            <svg xmlns="http://www.w3.org/2000/svg" width="46" height="44" viewBox="0 0 46 44"><g fill="none" stroke="#FFF" stroke-width="2">
+            <svg xmlns="http://www.w3.org/2000/svg" width="46" height="44"><g fill="none" stroke="#fff" strokeWidth="2">
                 <path d="M1 22.019C8.333 21.686 23 25.616 23 44M23 44V0M45 22.019C37.667 21.686 23 25.616 23 44"/></g></svg>
           </button>
         </form>
