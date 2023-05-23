2538a2da34e428cc213b6b8a79868df5ea2daaec
Scott Clingan <sclingan1@gmail.com>
2023-05-23 14:54:46 -0500

    Using React state for dates

--- src/age-calculator.tsx
+++ src/age-calculator.tsx
@@ -2,10 +2,5 @@
 
 export default function ageCalculator(e) {
     e.preventDefault();
-    let day: HTMLElement = document.getElementById('day');
-    let month: HTMLElement = document.getElementById('month');
-    let year: HTMLElement = document.getElementById('year');
-    console.log(day.value);
-    console.log(month.value);
-    console.log(year.value);
+
 }
--- src/form.tsx
+++ src/form.tsx
@@ -2,22 +2,24 @@ import { useState } from 'react'
 import ageCalculator from './age-calculator'
 
 function Form() {
-
+    const [day, setDay] = useState('');
+    const [month, setMonth] = useState('');
+    const [year, setYear] = useState('');
   return (
     <main>
       <h1 className='sr-only'>Age Calculator App</h1>
         <form>
           <div>
           <label htmlFor='day'>Day</label>
-          <input placeholder='DD' id='day'></input>
+          <input id='day' value={day} onChange={e => setDay(e.target.value)} placeholder='DD'></input>
           </div>
           <div>
           <label htmlFor='month'>Month</label>
-          <input placeholder='MM' id='month'></input>
+          <input id='month' value={month} onChange={e => setMonth(e.target.value)} placeholder='MM'></input>
           </div>
           <div>
           <label htmlFor='year'>Year</label>
-          <input placeholder='YYYY' id='year'></input>
+          <input id='year' value={year} onChange={e => setYear(e.target.value)} placeholder='YYYY'></input>
           </div>
           <button onClick={ageCalculator}>
             <svg xmlns="http://www.w3.org/2000/svg" width="46" height="44"><g fill="none" stroke="#fff" strokeWidth="2">
