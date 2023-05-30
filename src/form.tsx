3e7f608dddf8e22706abbd962db0babbe946e1b9
Scott Clingan <sclingan1@gmail.com>
2023-05-29 23:22:05 -0500

    Adding error messages

--- src/form.tsx
+++ src/form.tsx
@@ -39,17 +39,20 @@ function Form() {
           <div>
           <label htmlFor='day'>Day</label>
           <input id='day' value={day} onChange={e => setDay(e.target.value)} placeholder='DD'></input>
-          <p className='sr-only'>Must be a valid date</p>
+          <p className='sr-only' id='valid'>Must be a valid date</p>
+          <p className='sr-only' id='empty'>This field is required</p>
           </div>
           <div>
           <label htmlFor='month'>Month</label>
           <input id='month' value={month} onChange={e => setMonth(e.target.value)} placeholder='MM'></input>
-          <p className='sr-only'>Must be a valid month</p>
+          <p className='sr-only' id='valid'>Must be a valid month</p>
+          <p className='sr-only' id='empty'>This field is required</p>
           </div>
           <div>
           <label htmlFor='year'>Year</label>
           <input id='year' value={year} onChange={e => setYear(e.target.value)} placeholder='YYYY'></input>
-          <p className='sr-only'>Must be in the past</p>
+          <p className='sr-only' id='valid'>Must be in the past</p>
+          <p className='sr-only' id='empty'>This field is required</p>
           </div>
           <button onClick={getAge}>
             <svg xmlns="http://www.w3.org/2000/svg" width="46" height="44"><g fill="none" stroke="#fff" strokeWidth="2">
